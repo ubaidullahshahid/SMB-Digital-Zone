dbf2b3b9e8e928c60e920c3fd7f86c6e787d7df3
ubaidullahshahid <146563446+ubaidullahshahid@users.noreply.github.com>
2025-05-05 08:45:42 +0500

    change config.ts

--- config.ts
+++ config.ts
@@ -1,4 +1,4 @@
-export const Base_URL =
+export const BASE_URL =
   process.env.NODE_ENV === "production"
     ? "https://smb-digital-zone-backend.vercel.app/api"
     : "http://localhost:5000/api";
